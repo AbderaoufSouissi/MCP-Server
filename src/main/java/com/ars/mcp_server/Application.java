0df7063f4c7363296c2fa73aaeea96023da7e4d1
abderaouf souissi <abderaouf.souissi@outlook.com>
2025-11-02 16:19:39 +0100

    force app to run and disable logging

--- src/main/java/com/ars/mcp_server/Application.java
+++ src/main/java/com/ars/mcp_server/Application.java
@@ -1,13 +1,39 @@
 package com.ars.mcp_server;
 
+import com.ars.mcp_server.service.ProductService;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
-public class McpServerApplication {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(McpServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setLogStartupInfo (false);
+        app.setBannerMode(Banner.Mode.OFF);
+        ConfigurableApplicationContext context = app.run(args);
+
+        Runtime.getRuntime().addShutdownHook (new Thread(context::close));
+
+        try {
+            Thread.currentThread().join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+
+    @Bean
+    public ToolCallbackProvider toolCallbackProvider(ProductService productService) {
+        return MethodToolCallbackProvider.builder()
+                .toolObjects(productService)
+                .build();
+    }
 
 }
